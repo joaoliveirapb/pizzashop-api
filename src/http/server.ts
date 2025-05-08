1bbd8aa9b166e0ccc371604362f72491792ef523
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-08 12:37:30 -0300

    refactor: separate route to create restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,38 +1,7 @@
-import { Elysia, t } from 'elysia'
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { Elysia } from 'elysia'
+import { registerRestaurant } from './routes/register-restaurant'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, managerEmail, managerPhone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email: managerEmail,
-        phone: managerPhone,
-        role: 'manager',
-      })
-      .returning({ id: users.id })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager?.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      managerEmail: t.String({ format: 'email' }),
-      managerPhone: t.String(),
-    }),
-  }
-)
+const app = new Elysia().use(registerRestaurant)
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
