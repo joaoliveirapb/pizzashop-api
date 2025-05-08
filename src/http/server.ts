55197f0907cd96ada60a5f96c45b8c09f1357294
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-08 12:20:58 -0300

    feat: create route to create a restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,38 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World by Bun!'
-})
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, managerEmail, managerPhone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({
+        name: managerName,
+        email: managerEmail,
+        phone: managerPhone,
+        role: 'manager',
+      })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager?.id,
+    })
+
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      managerEmail: t.String({ format: 'email' }),
+      managerPhone: t.String(),
+    }),
+  }
+)
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
