1376a04c53657a5ba7119ca647333ad120087ac8
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-09 00:16:09 -0300

    feat: create route to authenticate via link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,12 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { authenticateFromLink } from './routes/authenticate-from-link'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
