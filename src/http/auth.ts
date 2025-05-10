189962c577af5763e4f9118df38ce9c3f2f6fc44
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-10 15:55:17 -0300

    feat: create route to get the logged in user

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -32,5 +32,18 @@ export const auth = new Elysia()
       signOut: () => {
         removeCookie('auth')
       },
+
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(cookie.auth?.value)
+
+        if (!payload) {
+          throw new Error('Unauthorized')
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
+      },
     }
   })
