f4035641501c1a8653722fb34b80bfc52c5dcca7
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-10 21:03:28 -0300

    feat: create route to get the restaurant managed by the logged-in manager

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
 import { Elysia, t } from 'elysia'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
@@ -11,6 +12,7 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurant)
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
