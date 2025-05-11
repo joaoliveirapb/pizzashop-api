f1c1d972498fa31dc8fc39b90aebb7952b575526
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-11 17:02:32 -0300

    feat: create email sending functionality in `authenticate` route

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -1,8 +1,10 @@
 import { createId } from '@paralleldrive/cuid2'
 import { Elysia, t } from 'elysia'
+import nodemailer from 'nodemailer'
 import { db } from '../../db/connection'
 import { authLinks } from '../../db/schema'
 import { env } from '../../env'
+import { mail } from '../../lib/mail'
 
 export const sendAuthLink = new Elysia().post(
   '/authenticate',
@@ -31,9 +33,14 @@ export const sendAuthLink = new Elysia().post(
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-    // Enviar um e-mail
+    const info = await mail.sendMail({
+      from: { name: 'Pizza Shop', address: 'hi@pizzashop.com' },
+      to: email,
+      subject: 'Authenticate to Pizza Shop',
+      text: `Use the following link to authenticate on Pizza Shop: ${authLink.toString()}`,
+    })
 
-    console.log(authLink.toString())
+    console.log(nodemailer.getTestMessageUrl(info))
   },
   {
     body: t.Object({
