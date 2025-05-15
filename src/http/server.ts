78b8c2c120f5e868af7219f47633ea32d32f235e
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-15 19:36:50 -0300

    feat: create route to approve the order

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,4 +1,5 @@
 import { Elysia } from 'elysia'
+import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
@@ -15,6 +16,7 @@ const app = new Elysia()
   .use(getProfile)
   .use(getManagedRestaurant)
   .use(getOrderDetails)
+  .use(approveOrder)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION': {
