7c1c3ca050823ac84c4816df0d9fa983b61fae6b
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-17 15:56:01 -0300

    feat: create route to get month canceled orders amount

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getDayOrdersAmount } from './routes/get-day-orders-amount'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getMonthCanceledOrdersAmount } from './routes/get-month-canceled-orders-amount'
 import { getMonthOrdersAmount } from './routes/get-month-orders-amount'
 import { getMonthReceipt } from './routes/get-month-receipt'
 import { getOrderDetails } from './routes/get-order-details'
@@ -31,6 +32,7 @@ const app = new Elysia()
   .use(getMonthReceipt)
   .use(getDayOrdersAmount)
   .use(getMonthOrdersAmount)
+  .use(getMonthCanceledOrdersAmount)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION': {
