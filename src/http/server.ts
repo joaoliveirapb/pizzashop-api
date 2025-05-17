76dc2aaca76e7de22bd88acb7d795f3559717ca1
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-05-16 21:31:30 -0300

    feat: create route to get current month receipt

--- src/http/server.ts
+++ src/http/server.ts
@@ -5,6 +5,7 @@ import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getMonthReceipt } from './routes/get-month-receipt'
 import { getOrderDetails } from './routes/get-order-details'
 import { getOrders } from './routes/get-orders'
 import { getProfile } from './routes/get-profile'
@@ -25,6 +26,7 @@ const app = new Elysia()
   .use(deliverOrder)
   .use(dispatchOrder)
   .use(getOrders)
+  .use(getMonthReceipt)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION': {
